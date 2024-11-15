79d296dc64c53a58af822ac36799d010a6d788da
voidkudo <voidkudo@gmail.com>
2024-11-15 02:01:39 -0500

    feat: import font

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import '@fontsource/unifontex/400.css';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import PageLayout from './components/PageLayout';
 import { CssBaseline } from '@mui/material';
