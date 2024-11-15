4d50b0c0e6fcb227b4dd105fafb8040ce81f56ba
voidkudo <voidkudo@gmail.com>
2024-11-15 02:04:22 -0500

    style: adjust layout div

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
+import { Box } from "@mui/material"
 
 const PageLayout = () => {
   return (
-    <>
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "auto"
+      }}
+    >
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Box>
   )
 }
 
