8493f1c46a4c7039fcb689ed9f6bc4907727b67e
voidkudo <voidkudo@gmail.com>
2024-11-15 02:03:38 -0500

    refactor: relocate type

--- src/pages/pageRoutes.tsx
+++ src/pages/pageRoutes.tsx
@@ -1,7 +1,19 @@
-import { Page, PageRoute } from "../types/pages.d";
+import { ReactNode } from "react";
 import AboutPage from "./AboutPage";
 import HomePage from "./HomePage";
 import PortfolioPage from "./PortfolioPage";
+
+export const enum Page {
+  Home = "Home",
+  About = "About",
+  Portfolio = "Portfolio",
+}
+
+export type PageRoute = {
+  pageName: Page,
+  path: string,
+  element: ReactNode,
+}
 
 export const PageRoutes: PageRoute[] = [
   {
