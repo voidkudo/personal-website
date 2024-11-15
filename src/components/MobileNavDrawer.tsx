b4f3c679c600eaf84c58aaa7b47c53e2235aa00d
voidkudo <voidkudo@gmail.com>
2024-11-15 02:03:56 -0500

    style: remove empty lines

--- src/components/MobileNavDrawer.tsx
+++ src/components/MobileNavDrawer.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom"
 export type MobileNavDrawerHandle = {
   handleDrawerOpen: () => void,
   handleDrawerClose: () => void,
-
 }
 
 const MobileNavDrawer = forwardRef<MobileNavDrawerHandle, DrawerProps>((props, ref) => {
