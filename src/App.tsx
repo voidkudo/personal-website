08fc03a6acf8b03e77b57323491c9382cb860bb3
voidkudo <voidkudo@gmail.com>
2024-11-18 02:46:13 -0500

    build: add deploy

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const theme = createTheme({
 
 const router = createBrowserRouter([
   {
-    path: "",
+    path: "/personal-website",
     element: <PageLayout />,
     errorElement: <ErrorPage />,
     children: PageRoutes.map(route => {
